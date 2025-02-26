da90682b51f8675f1cf7f93399a791c6de9d19ec
KIRIAKE Masanori <seiken@rd01.net>
2025-02-26 22:15:36 +0900

    とりあえずデモができる。

--- sudachibrl.py
+++ sudachibrl.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from sudachipy import tokenizer
 from sudachipy import dictionary
 import braillel_rules_pb2
@@ -6,13 +9,12 @@ import sys
 tokenizer_obj = dictionary.Dictionary().create()
 mode = tokenizer.Tokenizer.SplitMode.B
 
+print("Input Japanese sentense and hit enter key!")
 for line in sys.stdin:
     line = line.strip()
     list = tokenizer_obj.tokenize(line, mode)
     for m in list:
         print(f"{m.reading_form()} ", end="")
+    print("\n")
 
-print("\n")
-
-    
-
+print("End")

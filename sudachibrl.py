d9e9e06959ffe7ab684b8234265cf1b154d6c491
KIRIAKE Masanori <seiken@rd01.net>
2025-02-26 12:33:07 +0000

    fix newline problem.

--- sudachibrl.py
+++ sudachibrl.py
@@ -1,11 +1,17 @@
 from sudachipy import tokenizer
 from sudachipy import dictionary
 import sys
+
 tokenizer_obj = dictionary.Dictionary().create()
 mode = tokenizer.Tokenizer.SplitMode.B
 
 for line in sys.stdin:
     line = line.strip()
     list = tokenizer_obj.tokenize(line, mode)
-    print( [m.reading_form() for m in list] )
+    for m in list:
+        print(f"{m.reading_form()} ", end="")
+
+print("\n")
+
+    
 

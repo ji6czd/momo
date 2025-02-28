ec8e8c6954c0d0db8454ec1a914c14c8700ce921
KIRIAKE Masanori <seiken@rd01.net>
2025-03-01 01:38:27 +0900

    File add & deleted.

--- braille_rules.py
+++ braille_rules.py
@@ -1,7 +1,7 @@
 import braille_rules_pb2
 from google.protobuf import text_format
 import sys
-braille_rules = braille_rules_pb2.BrailleRules()
+rules = braille_rules_pb2.BrailleRules()
 
 def CreateBrailleRules():
     rules = braille_rules_pb2.BrailleRules()
@@ -15,19 +15,16 @@ def CreateBrailleRules():
 
 def WriteBrailleRules():
     with open('backup.textproto', 'w') as f:
-        f.write(text_format.MessageToString(braille_rules))
+        f.write(text_format.MessageToString(rules))
 
 def LoadBrailleRules():
     try:
         with open('braille_rules.textproto', 'r') as f:
-            text_format.Parse(f.read(), braille_rules)
+            text_format.Parse(f.read(), rules)
     except FileNotFoundError:
         print("Error: The file 'braille_rules.textproto' was not found.", file=sys.stderr)
     except IOError as e:
         print(f"Error: An I/O error occurred: {e}", file=sys.stderr)
-
-def ProcessBrailleRules(inString):
-    return inString
 
 if __name__ == '__main__':
     LoadBrailleRules()

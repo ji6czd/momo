6d155662817dca3ce4c5fdbe6567907acc8b91ad
KIRIAKE Masanori <seiken@rd01.net>
2025-03-02 19:49:53 +0900

    * 品詞を検索する際、最も詳細な情報にアクセスできるように変更。 * コア部分をCLIと分離し、momo_core.pyに移動。 * 点訳ルールを更新。

--- momo.py
+++ momo.py
@@ -1,54 +1,14 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
-from sudachipy import tokenizer
-from sudachipy import dictionary
-import braille_rules
+from momo_core import convert_to_kana
 import pybraille
 import sys
-
-mode = tokenizer.Tokenizer.SplitMode.B
-tokenizer_obj = dictionary.Dictionary().create(mode)
-braille_rules.LoadBrailleRules()
-
-def searchBrailleRule(current_morpheme, next_morpheme):
-    space_flag = True
-    for rule in braille_rules.rules.rule:
-        if current_morpheme.part_of_speech()[0] == rule.current_pos.name:
-            for n_rule in rule.next_pos:
-                if next_morpheme.part_of_speech()[0] == n_rule.name:
-                    space_flag = n_rule.before_space
-                    break
-    return space_flag
-
-def ProcessBrailleRules(tokenizedList):
-    kanaString = ""
-    for m_index, m in enumerate(tokenizedList):
-        # 助詞を点字のルールに変換する
-        if m.part_of_speech()[0] == "助詞":
-            if m.reading_form() == "ハ":
-                kanaString += "ワ"
-            elif m.reading_form() == "ヘ":
-                kanaString += "エ"
-            else:
-                kanaString += m.reading_form()
-        elif m.part_of_speech()[1] == "数詞" or m.part_of_speech()[0] == "補助記号":
-            kanaString += m.surface()
-        else:
-            # それ以外の品詞は、そのまま読みを追加する
-            kanaString += m.reading_form()
-        # この品詞のルールを、ルールリストに基づいて処理する
-        if m_index < len(tokenizedList) - 1:
-            if searchBrailleRule(m, tokenizedList[m_index + 1]):
-                kanaString += " "
-    return kanaString
 
 if __name__ == '__main__':
     print("Input Japanese sentence and hit enter key!")
     for line in sys.stdin:
         line = line.strip()
-        list = tokenizer_obj.tokenize(line, mode)
-        kana_string = ProcessBrailleRules(list)
+        kana_string = convert_to_kana(line)
         print(line)
         print(kana_string)
         print(pybraille.to_jp_braille(kana_string))

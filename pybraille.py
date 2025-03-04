3b7c2b47f166aa67dfaa57d8ac523ec47170dd5d
KIRIAKE Masanori <seiken@rd01.net>
2025-03-05 04:11:06 +0900

    * 英字列を変換できるようにしてみた。

--- pybraille.py
+++ pybraille.py
@@ -12,6 +12,8 @@ en_braille_table = {
     'u': '⠥', 'v': '⠧', 'w': '⠺', 'x': '⠭', 'y': '⠽', 'z': '⠵',}
 
 numerical_sign = '⠼'
+capital_sign = '⠠'
+foreign_word_sign = '⠰'
 
 jp_braille_table = {
     ' ': '⠀', 'ア': '⠁', 'イ': '⠃', 'ウ': '⠉', 'エ': '⠋', 'オ': '⠊',
@@ -57,9 +59,13 @@ def to_jp_braille(text):
     braille_str = ""
     inDigitFlag = False # 数字中にTrue
     inCapitalWordFlag = False # 英文字列中 True
+    inDoubleCapitalFlag = False # ２重大文字状態
+    inForeignWordFlag = False # 単語が外国語であることを表すフラグ
     skip = False # 特殊音で次の文字をスキップ
+
     for index, c in enumerate(normalized_text):
         if skip:
+            # 特殊音などで直前のループで処理済みの文字をスキップ
             skip = False
             continue
         if c.isdigit():
@@ -71,7 +77,33 @@ def to_jp_braille(text):
         else:
             inDigitFlag = False
         # １文字ずつ点字に変換
-        if (index+1 < len(normalized_text)
+        if ord(c) < 0x7f and c.isalpha():
+            # 英数字処理、まずは外字符
+            if inForeignWordFlag == False:
+                braille_str += foreign_word_sign
+                inForeignWordFlag = True
+
+            # 英字でなければinForeignWordFlagの効力がなくなる
+            if (inForeignWordFlag == True
+                and c.isalpha()) == False:
+                inForeignWordFlag = False
+            if c.isupper():
+                if inCapitalWordFlag == False:
+                    braille_str += capital_sign
+                    inCapitalWordFlag = True
+                    if (index < len(normalized_text)-1
+                        and normalized_text[index+1].isupper()):
+                        inDoubleCapitalFlag = True
+                        braille_str += capital_sign
+                c = c.lower()
+            else:
+                if inDoubleCapitalFlag == True:
+                    inDoubleCapitalFlag = False
+                    braille_str += foreign_word_sign
+                inCapitalWordFlag = False
+            braille_str += en_braille_table.get(c, '⠀')
+            continue
+        elif (index+1 < len(normalized_text)
             and small_kana.find(normalized_text[index+1]) != -1):
             # 特殊文字処理
             c = normalized_text[index] + normalized_text[index + 1]
@@ -80,8 +112,9 @@ def to_jp_braille(text):
     return braille_str
 
 if __name__ == '__main__':
-    print(to_braille('Hello World!.'))
+    print(to_jp_braille('Hello World!.'))
     print(to_jp_braille("コンニチワ、セカイ！"))
     print(to_jp_braille("12345"))
     print(to_jp_braille("３ネン ２クミ"))
     print(to_jp_braille("ワタシワ ベンキョーガ キライナノデチュ。ヴァイオリンノ レンシューワ モット キライニャニェニョーー。"))
+    print(to_jp_braille("NHK ラジオ ダイ１ノ シューハスーワ ５９４kHz デス。"))

b5b9014672a5e0b541b72d4026dfbc1fc859edfc
KIRIAKE Masanori <seiken@rd01.net>
2025-04-08 17:54:27 +0900

    Packaging again.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     version="0.0.1",  # バージョン
     description="Japanese Text to Braille Converter Library",  # 説明
     author='KIRIAKE Masanori',  # 作者名
+    include_package_data=True,
     packages=find_packages(),  # 使うモジュール一覧
     install_requires=[
         'protobuf',  # Protobufに依存

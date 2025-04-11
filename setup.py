c889ea05ede9744f270d2ae1b670bb55cafa7f6e
KIRIAKE Masanori <seiken@rd01.net>
2025-04-11 17:02:53 +0900

    * インストール時の依存関係を外した。 * Protobufのバージョンが開発側より高いとエラーになるため、実行側で適切なバージョンを入れてほしい。 * いずれは、setup.pyの方でバージョンを指定するようにしたい。

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 from setuptools import setup, find_packages
 setup(
     name='momoPy',  # パッケージ名（pip listで表示される）
-    version="0.0.1",  # バージョン
+    version="0.0.2",  # バージョン
     description="Japanese Text to Braille Converter Library",  # 説明
     author='KIRIAKE Masanori',  # 作者名
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'protobuf',  # Protobufに依存
-        'SudachiPy',  # SudachiPyに依存
-        'SudachiDict_core',  # SudachiDict_coreに依存
-    ],
     license='Apach'  # ライセンス
 )

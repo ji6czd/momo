5199c78f165f12eac79f7695e5666d2333edb2e6
KIRIAKE Masanori <seiken@rd01.net>
2025-04-14 16:59:45 +0900

    * Update 0.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='momoPy',  # パッケージ名（pip listで表示される）
-    version="0.0.2",  # バージョン
+    version="0.0.3",  # バージョン
     description="Japanese Text to Braille Converter Library",  # 説明
     author='KIRIAKE Masanori',  # 作者名
     packages=find_packages(),

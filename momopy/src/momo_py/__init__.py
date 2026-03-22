c064675872cc541f62dec42b1e995cd216564e6f
KIRIAKE Masanori <seiken@rd01.net>
2026-03-22 15:51:11 +0900

    * PredictorConfigがインポートできなかったのを修正

--- momopy/src/momo_py/__init__.py
+++ momopy/src/momo_py/__init__.py
@@ -1,9 +1,10 @@
 from .translator import Translator
 from .pybraille import to_jp_braille, to_braille
-from .predictor import Predictor, PredictionResult
+from .predictor import Predictor, PredictionResult, PredictorConfig
 
 __all__ = [
     "Translator",
     "Predictor",
     "PredictionResult",
+    "PredictorConfig",
 ]

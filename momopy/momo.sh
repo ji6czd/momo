8095d13220691e54541b82992e319c66295bcc29
KIRIAKE Masanori <seiken@rd01.net>
2026-04-30 13:45:42 +0900

    * 前モデルビルドコマンド追加。

--- momopy/momo.sh
+++ momopy/momo.sh
@@ -2,35 +2,48 @@
 set -eu
 raw=../dataset/basic_raw.tsv
 tsv=../dataset/basic_data.tsv
-model=../dataset/basic_data.zip
+model=../dataset/basic_data
 cmd=momo
 # 学習用関数
 check() {
     echo "Training..."
-    uv run $cmd createdata --raw=$raw
+    uv run ${cmd} createdata --raw=${raw}
 }
 
 train() {
     echo "Training..."
-    uv run $cmd createdata --raw=$raw
-    uv run $cmd train --tsv=$tsv
+    uv run ${cmd} createdata --raw=${raw}
+    uv run ${cmd} train --tsv=${tsv} --window 7
+    mv ${model}.zip ${model}_7.zip
 }
 
 # 予測用関数
 predict() {
     echo "Predicting..."
-    uv run $cmd predict --model=$model
+    uv run ${cmd} predict --model=${model}_7.zip
 }
 
 translate() {
     echo "Translating..."
-    uv run $cmd translate
+    uv run ${cmd} translate
 }
 
 label() {
     echo "Labeling..."
-    uv run $cmd label --model=$model
+    uv run ${cmd} label --model=${model}_7.zip
 }
+
+build_model() {
+    echo "Building model..."
+    uv run ${cmd} createdata --raw=${raw}
+    uv run ${cmd} train --tsv=${tsv} --window 7
+    mv ${model}.zip ${model}_7.zip
+    uv run ${cmd} train --tsv=${tsv} --window 5
+    mv ${model}.zip ${model}_5.zip
+    uv run ${cmd} train --tsv=${tsv} --window 3
+    mv ${model}.zip ${model}_3.zip
+}
+
 # メイン処理
 case "$1" in
     train)
@@ -48,8 +61,11 @@ case "$1" in
     label)
         label
         ;;
+    build)
+        build_model
+        ;;
     *)
-        echo "Usage: $0 {train|predict}"
+        echo "Usage: ${0} {train|predict}"
         exit 1
         ;;
 esac

faf3ead6614b02f269a884136953a9d85e46543e
KIRIAKE Masanori <seiken@rd01.net>
2026-08-27 00:18:07 +0900

    Version 0.10.0 Release!

--- web-api/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set Ceu
-service_name=neomomo
-gh run download --dir ./artifacts/ -n linux-dist
-gcloud run deploy $service_name --region=asia-northeast1 --memory=256Mi --concurrency=1 --source=. --set-build-env-vars=GOOGLE_PYTHON_PACKAGE_MANAGER=uv
